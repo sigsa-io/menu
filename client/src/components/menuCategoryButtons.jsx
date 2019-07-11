b01509516287e23044a92491fac0d55595e7af9d
Minho Choo <mchoo7@gatech.edu>
2019-07-10 19:03:09 -0700

    add basic enzyme test

--- client/src/components/menuCategoryButtons.jsx
+++ client/src/components/menuCategoryButtons.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import propTypes from 'prop-types';
 
 const MenuCategoryButtons = ({ restaurants, currentSubMenu, handleMenuCategoryClick }) => (
-  <div>
+  <div className="submenu-buttons-nav">
     { restaurants.map(category => (
       category.submenu_name === currentSubMenu
         ? <button className="button-menu-link-active__3KrRGgFS button-menu-link__1j5RFIKk secondary__37FrTgAl base__1GdpdUto" type="button" id={category.submenu_name} onClick={handleMenuCategoryClick}>{category.submenu_name}</button>

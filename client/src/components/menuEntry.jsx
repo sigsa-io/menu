bf7dcfb437cd614eab31df6d6c7892fd44808665
Minho Choo <mchoo7@gatech.edu>
2019-07-11 17:41:32 -0700

    add menuEntry map function test

--- client/src/components/menuEntry.jsx
+++ client/src/components/menuEntry.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import propTypes from 'prop-types';
 
 const MenuEntry = ({ entries }) => (
-  entries.map(entry => (
+  entries.map(({ menu_price, menu_name, menu_description }) => (
     <div className="menu-item__2ZxJOnTY">
-      <div className="menu-price__1KiCimAr">{entry.menu_price}</div>
-      <div className="entry-item-name">{entry.menu_name}</div>
-      <div className="menu-description__2HXkC4oE">{entry.menu_description}</div>
+      <div className="menu-price__1KiCimAr">{menu_price}</div>
+      <div className="entry-item-name">{menu_name}</div>
+      <div className="menu-description__2HXkC4oE">{menu_description}</div>
     </div>
   ))
 );

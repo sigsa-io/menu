511ec3bc7ecbfef047e64e9c1ba3d9375dd1ecf7
Minho Choo <mchoo7@gatech.edu>
2019-07-12 23:28:24 -0700

    build circleci test and add full dir to import file

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
-import MenuCategoryButtons from '../../components/MenuCategoryButtons.jsx';
-import MenusByHeaders from '../../components/MenusByHeaders.jsx';
+import MenuCategoryButtons from './menuCategoryButtons.jsx';
+import MenusByHeaders from './menusByHeaders.jsx';
 import style from './style/App.style.css';
 
 class App extends React.Component {
